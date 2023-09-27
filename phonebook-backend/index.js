849cd404226ec05b0d67d45480af3842565c38bc
Tiago Ferreira <tiago@okteto.com>
2023-09-27 22:55:05 +0200

    3.16: Phonebook database, step4

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -53,22 +53,29 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find().then(person => {
-    response.json(person)
-  })
-})
-
-app.get('/api/persons/:id', (request, response) => {
   Person
-    .findById(request.params.id)
+    .find()
     .then(person => {
       response.json(person)
     })
-    .catch(() => response.status(404).end())
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => next(error))
  
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person
     .findByIdAndRemove(request.params.id)
     .then(result => {
@@ -108,7 +115,28 @@ app.post('/api/persons', (request, response) => {
       .then(savedPerson => {
         response.json(savedPerson)
       })
+      .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
