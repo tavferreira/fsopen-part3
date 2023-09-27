aec65cf43801a9a64ac7bf0dcb2918d5d2e632fc
Tiago Ferreira <tiago@okteto.com>
2023-09-27 23:31:58 +0200

    3.20*: Phonebook database, step8

--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -22,7 +22,14 @@ const personSchema = new mongoose.Schema({
     type: String,
     minLength: 3,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => /\d{2,3}-\d{6,}/.test(v),
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  },
 })
 
 personSchema.set('toJSON', {
